1f9189346ecec11082f174de10708437fe82cac0
henry <henrysilvatapullima38@gmail.com>
2025-12-26 16:16:45 -0500

    fix: remove deprecated eslint config for Next 16

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,5 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true, // ⭐ Permite build aunque haya errores de ESLint
-  },
-};
+import type { NextConfig } from "next";
 
-module.exports = nextConfig;
+const nextConfig: NextConfig = {};
+
+export default nextConfig;
