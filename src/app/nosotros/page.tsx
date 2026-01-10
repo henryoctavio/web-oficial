25c9208e491800b98eae7f98f1a0260d3e898048
Henry Silva <henrysilvatapullima@gmail.com>
2026-01-09 22:44:15 -0500

    Estamos trabajando para ofrecerte una mejor interactividad con nuestro sitio web.

--- src/app/nosotros/page.tsx
+++ src/app/nosotros/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Nosotros = () => {
-  return <div className="text-[rgb(141,110,235)] mt-16">Nosotros</div>;
+  return <div className="text-[rgb(141,110,235)] mt-16">Hombres trabajando</div>;
 };
 
 export default Nosotros;
