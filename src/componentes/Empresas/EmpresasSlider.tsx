0b4d16e7f7e0ed2e44db7af98af0e9c5b256822f
Henry Silva <henrysilvatapullima@gmail.com>
2026-01-13 14:59:02 -0500

    change EmpresasSlider background to gradient

--- src/componentes/Empresas/EmpresasSlider.tsx
+++ src/componentes/Empresas/EmpresasSlider.tsx
@@ -20,7 +20,7 @@ const logos = [
 const EmpresasSlider = () => {
   return (
     <div>
-      <div className="bg-black mt-10 sm:mt-6 md:mt-10 lg:mt-16 w-full py-8 md:py-12 flex justify-center items-center">
+      <div className="bg-gradient-to-r from-[#030014] via-purple-700 to-[#030014] mt-10 sm:mt-6 md:mt-10 lg:mt-16 w-full py-8 md:py-12 flex justify-center items-center">
         <Marquee>
           {logos.map((logo, index) => (
             <div key={index} className="mx-8 flex items-center relative w-20 h-10 sm:w-24 sm:h-12 md:w-32 md:h-16">
